147c6ceef7d265f4e3be5ccdfd8e7d2eed674ff8
kyotoplaya <homarucom@gmail.com>
2026-09-05 22:46:40 +0300

    Реализованы методы PUT и PATCH для categories

--- routes/categories.py
+++ routes/categories.py
@@ -17,13 +17,35 @@ def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     db.refresh(db_category)
     return db_category
 
+
 @router.get("/categories", response_model=list[Category])
 def get_categories(db: Session = Depends(get_db)):
     return db.query(CategoryModel).all()
 
+
 @router.get("/categories/{category_id}", response_model=Category)
 def get_category(category_id: int, db: Session = Depends(get_db)):
-    answer = db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    answer = db.query(CategoryModel).filter(
+        CategoryModel.id == category_id).first()
     if answer is None:
         raise HTTPException(status_code=404, detail="Category not found")
     return answer
+
+
+@router.patch("/categories/{category_id}", response_model=Category)
+@router.put("/categories/{category_id}", response_model=Category)
+def put_patch_category(category_id: int, category: CategoryCreate, db: Session = Depends(get_db)):
+    category_to_update = db.query(CategoryModel).filter(
+        CategoryModel.id == category_id).first()
+
+    if category_to_update is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+    updated_category = category.model_dump()
+    for key, value in updated_category.items():
+        setattr(category_to_update, key, value)
+
+    db.commit()
+    db.refresh(category_to_update)
+
+    return category_to_update
