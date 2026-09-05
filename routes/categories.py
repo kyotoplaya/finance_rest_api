7035e2ebecae5368460eb73f9c9986a89a150fb9
kyotoplaya <homarucom@gmail.com>
2026-09-05 22:17:56 +0300

    Реализованы методы POST и GET для категорий\категорим по id

--- routes/categories.py
+++ routes/categories.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -16,3 +16,14 @@ def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_category)
     return db_category
+
+@router.get("/categories", response_model=list[Category])
+def get_categories(db: Session = Depends(get_db)):
+    return db.query(CategoryModel).all()
+
+@router.get("/categories/{category_id}", response_model=Category)
+def get_category(category_id: int, db: Session = Depends(get_db)):
+    answer = db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if answer is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return answer
